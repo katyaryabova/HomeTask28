e984791f6a9fc8b15c0c5d03f68c89abc5a31407
Ekaterina <37583993+katyaryabova@users.noreply.github.com>
2020-08-31 02:36:25 +0300

    Update Task3 Create a new file.py

--- Task3 Create a new file.py
+++ Task3 Create a new file.py
@@ -6,11 +6,15 @@ with open('txt3') as grades_file:
         grades = [float(k) for k in grades.split()]
         avg = sum(grades) / len(grades)
         print("{} has an average grade of {:.2f}.".format(name, avg))
+grades_file.close()
+print()
 
 for line in open('txt3', 'r', encoding='utf-8'):
     name, grades = re.split(":+", line)
+    first_name, last_name = re.split(" ", name)
+    short_name = last_name + ' ' + first_name[0] + '.'
     grades = [float(k) for k in grades.split()]
-    avg = sum(grades) / len(grades)
-    if avg == int(avg):
-        avg = int(avg)
-    open('txt29', 'a').write(f'{name, avg}\n')
+    avg = str(round(sum(grades) / len(grades),2))
+    open('txt34j', 'a').write(f'{ short_name  + " "  +  avg}\n')
+
+
