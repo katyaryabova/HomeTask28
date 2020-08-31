36c48e08341223fa100716e71479e65197958915
Ekaterina <37583993+katyaryabova@users.noreply.github.com>
2020-08-31 13:02:54 +0300

    Update Task3 Create a new file.py

--- Task3 Create a new file.py
+++ Task3 Create a new file.py
@@ -1,6 +1,6 @@
 import re
 
-with open('txt3') as grades_file:
+with open('txt new') as grades_file:
     for line in grades_file:
         name, grades = re.split(":+", line)
         grades = [float(k) for k in grades.split()]
@@ -9,12 +9,11 @@ with open('txt3') as grades_file:
 grades_file.close()
 print()
 
-for line in open('txt3', 'r', encoding='utf-8'):
+for line in open('txt new', 'r', encoding='utf-8'):
     name, grades = re.split(":+", line)
     first_name, last_name = re.split(" ", name)
     short_name = last_name + ' ' + first_name[0] + '.'
     grades = [float(k) for k in grades.split()]
     avg = str(round(sum(grades) / len(grades),2))
-    open('txt34j', 'a').write(f'{ short_name  + " "  +  avg}\n')
-
+    open('txt update8', 'a').write(f'{ "{0}{1}{2}".format(short_name, " ",  avg)}\n')
 
